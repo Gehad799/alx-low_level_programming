53b1c6353f8884414f8541bbbdfff9efd774c1e8
Gehad Alaa <125475192+Gehad799@users.noreply.github.com>
2023-04-03 00:34:30 +0200

    Add files via upload

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - this is function to calc sum
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - a-z
+ *
+ * Discription : a-z (10 times)
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch, i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
