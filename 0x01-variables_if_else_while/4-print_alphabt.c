527236d9b6448443e5e14183ba896371d9dfc946
Gehad799 <gehadalaa799@gmail.com>
2023-04-02 14:56:11 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,14 +10,17 @@
 
 int main(void)
 {
-	char low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (low != 'e' && low != 'q')
+		if (low != e && low != q)
 			putchar(low);
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
